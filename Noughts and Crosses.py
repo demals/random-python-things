6689400b2f63be50a547a2ee7c7379eed21dee04
demals <45132992+demals@users.noreply.github.com>
2018-12-17 14:51:08 +0000

    Add files via upload
    This one is semi-smart

--- Noughts and Crosses.py
+++ Noughts and Crosses.py
@@ -1,7 +1,7 @@
 import random
 
 def menu():
-    print("Noughts and Crosses\n1.Player Vs Player\n2.Player Vs Computer\n3.Quit")
+    print("Noughts and Crosses\n1.Player Vs Computer\n2.Quit")
     user = int(input())
     return user
 
@@ -36,8 +36,6 @@ def picker_2(symbol):
 def user(symbol,grid,p_number):
     user  = int(input("Player " + str(p_number) + " what space do you want? "))
     while True:
-        if user == 0:
-            user = int(input("you can't have that dude, pick another space: "))
         if grid[user] == " ":
             grid[user] = symbol
             return grid
@@ -50,39 +48,90 @@ def player(player_number):
     else:
         return 2
 
-def pvp():
-    grid = [" ", " ", " ", " ", " ", " ", " ", " ", " "," "]
-    grid_help()
-    player_1_symbol = picker_1()
-    player_2_symbol = picker_2(player_1_symbol)
-    player_number = random.randint(1,3)
-    turn = 0
-    while True:
-        turn += 1
-        grid_printer(grid)
-        player_number = player(player_number)
-        if player_number == 1:
-            user(player_1_symbol,grid,player_number)
-        else:
-            user(player_2_symbol,grid,player_number)
-        win = win_con(grid)
-        if win == "win":
-            grid_printer(grid)
-            print("Player " + str(player(player_number)) + " is the Winner!")
-            break
-        if turn == 9:
-            grid_printer(grid)
-            print("Tie")
-            break
-        player_number += 1
-
 def cpu(symbol,grid):
+    return ai(symbol,grid)
+
+def random_block(symbol,grid):
     while True:
-        i = random.randint(1,9)
+        i = random.randint(1, 10)
         if grid[i] == " ":
             grid[i] = symbol
-            return grid
+            return "ok"
 
+def attack(symbol,grid):
+    if (((grid[1] == grid[4]) and grid[1] == symbol) or ((grid[3] == grid[5]) and grid[3] == symbol) or ((grid[8] == grid[9]) and grid[8] == symbol)) and grid[7] == " ":
+        grid[7] = symbol
+        return "ok"
+    elif (((grid[2] == grid[3]) and grid[2] == symbol) or ((grid[8] == grid[9]) and grid[8] == symbol) or ((grid[4] == grid[7]) and grid[4] == symbol)) and grid[1] == " ":
+        grid[1] = symbol
+        return "ok"
+    elif (((grid[1] == grid[5]) and grid[1] == symbol) or ((grid[3] == grid[6]) and grid[3] == symbol) or ((grid[8] == grid[7]) and grid[8] == symbol)) and grid[9] == " ":
+        grid[9] = symbol
+        return "ok"
+    elif (((grid[1] == grid[2]) and grid[1] == symbol) or ((grid[7] == grid[5]) and grid[7] == symbol) or ((grid[6] == grid[9]) and grid[6] == symbol)) and grid[3] == " ":
+        grid[3] = symbol
+        return "ok"
+    elif (((grid[1] == grid[3]) and grid[1] == symbol) or ((grid[8] == grid[5]) and grid[5] == symbol)) and grid[2] == " ":
+        grid[2] = symbol
+        return "ok"
+    elif (((grid[1] == grid[7]) and grid[1] == symbol) or ((grid[6] == grid[5]) and grid[5] == symbol)) and grid[4] == " ":
+        grid[4] = symbol
+        return "ok"
+    elif (((grid[1] == grid[7]) and grid[1] == symbol) or ((grid[6] == grid[5]) and grid[5] == symbol)) and grid[4] == " ":
+        grid[4] = symbol
+        return "ok"
+    elif (((grid[4] == grid[5]) and grid[4] == symbol) or ((grid[3] == grid[9]) and grid[3] == symbol)) and grid[6] == " ":
+        grid[6] = symbol
+        return "ok"
+    elif (((grid[2] == grid[4]) and grid[2] == symbol) or ((grid[7] == grid[9]) and grid[9] == symbol)) and grid[8] == " ":
+        grid[8] = symbol
+        return "ok"
+    elif (((grid[1] == grid[9]) and grid[1] == symbol) or ((grid[2] == grid[8]) and grid[2] == symbol) or ((grid[7] == grid[3]) and grid[7] == symbol) or ((grid[6] == grid[4]) and grid[6] == symbol)) and grid[5] == " ":
+        grid[5] = symbol
+        return "ok"
+    else:
+        return None
+def block(symbol,grid):
+    if (((grid[1] == grid[4]) and grid[1] != symbol) or ((grid[3] == grid[5]) and grid[3] != symbol) or ((grid[8] == grid[9]) and grid[8] != symbol)) and grid[7] == " ":
+        grid[7] = symbol
+        return "ok"
+    elif (((grid[2] == grid[3]) and grid[2] != symbol) or ((grid[8] == grid[9]) and grid[8] != symbol) or ((grid[4] == grid[7]) and grid[4] != symbol)) and grid[1] == " ":
+        grid[1] = symbol
+        return "ok"
+    elif (((grid[1] == grid[5]) and grid[1] != symbol) or ((grid[3] == grid[6]) and grid[3] != symbol) or ((grid[8] == grid[7]) and grid[8] != symbol)) and grid[9] == " ":
+        grid[9] = symbol
+        return "ok"
+    elif (((grid[1] == grid[2]) and grid[1] != symbol) or ((grid[7] == grid[5]) and grid[7] != symbol) or ((grid[6] == grid[9]) and grid[6] != symbol)) and grid[3] == " ":
+        grid[3] = symbol
+        return "ok"
+    elif (((grid[1] == grid[3]) and grid[1] != symbol) or ((grid[8] == grid[5]) and grid[5] != symbol)) and grid[2] == " ":
+        grid[2] = symbol
+        return "ok"
+    elif (((grid[1] == grid[7]) and grid[1] != symbol) or ((grid[6] == grid[5]) and grid[5] != symbol)) and grid[4] == " ":
+        grid[4] = symbol
+        return "ok"
+    elif (((grid[1] == grid[7]) and grid[1] != symbol) or ((grid[6] == grid[5]) and grid[5] != symbol)) and grid[4] == " ":
+        grid[4] = symbol
+        return "ok"
+    elif (((grid[4] == grid[5]) and grid[4] != symbol) or ((grid[3] == grid[9]) and grid[3] != symbol)) and grid[6] == " ":
+        grid[6] = symbol
+        return "ok"
+    elif (((grid[2] == grid[4]) and grid[2] != symbol) or ((grid[7] == grid[9]) and grid[9] != symbol)) and grid[8] == " ":
+        grid[8] = symbol
+        return "ok"
+    elif (((grid[1] == grid[9]) and grid[1] != symbol) or ((grid[2] == grid[8]) and grid[2] != symbol) or ((grid[7] == grid[3]) and grid[7] != symbol) or ((grid[6] == grid[4]) and grid[6] != symbol)) and grid[5] == " ":
+        grid[5] = symbol
+        return "ok"
+    else:
+        return None
+
+def ai(symbol,grid):
+    if attack(symbol,grid) != None:
+        return "ok"
+    elif block(symbol,grid) != None:
+        return "ok"
+    else:
+        return random_block()
 def pve():
     grid = [" ", " ", " ", " ", " ", " ", " ", " ", " "," "]
     grid_help()
@@ -134,9 +183,7 @@ def win_con(grid):
 def main():
     user = menu()
     if user == 1:
-        pvp()
-    if user == 2:
         pve()
-    if user == 3:
+    if user == 2:
         print("Bye Bye!")
 main()
